1a50b45ba5554fcd1cf430359d4644b3368e022d
Julien Deschamps <jdeschamps34@yahoo.fr>
2026-03-06 15:22:51 +0100

    Sauvegard3

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\OrderRepository;
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,5 +17,44 @@ class AccountController extends AbstractController
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         return $this->render('account/index.html.twig');
+    }
+    
+    #[Route('/account/orders', name: 'account_orders')]
+    public function orders(OrderRepository $orderRepository): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $orders = $orderRepository->findBy(
+            ['user' => $this->getUser()],
+            ['id' => 'DESC']
+        );
+
+        return $this->render('account/orders.html.twig', [
+            'orders' => $orders,
+        ]);
+    }
+
+    #[Route('/account/orders/{id}', name: 'account_order_show')]
+    public function showOrder(
+        int $id,
+        EntityManagerInterface $em
+    ): Response {
+
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $order = $em->getRepository(Order::class)->find($id);
+
+        if (!$order) {
+            throw $this->createNotFoundException('Commande introuvable');
+        }
+
+        // sécurité : vérifier que la commande appartient à l'utilisateur
+        if ($order->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        return $this->render('account/order_show.html.twig', [
+            'order' => $order
+        ]);
     }
 }
